9dabfdcf33966556c9ae204485d31f4d458c5df3
Learn Forward 2023 <learnforward2023@gmail.com>
2024-02-11 16:19:43 +0900

    🔧 Base class structure.

--- src/Base/Base.h
+++ src/Base/Base.h
@@ -5,4 +5,26 @@
 #ifndef SKY_BIRDS_GAME_BASE_H
 #define SKY_BIRDS_GAME_BASE_H
 
+#include "../common/Common.h"
+
+class Base {
+  private:
+    int _xPos, _yPos;
+    SDL_Rect _rect;
+    SDL_Texture* _texture;
+
+  protected:
+    SDL_Surface* LoadSurface(const std::string &path);
+
+  public:
+    Base();
+    ~Base();
+
+    void SetRect(const SDL_Rect &rect) { _rect = rect; }
+
+    void Free();
+    bool LoadTexture(const std::string &path, SDL_Renderer* screen);
+    void Render(SDL_Renderer* des, SDL_Rect *clip = nullptr, SDL_Rect *rect = nullptr);
+};
+
 #endif //SKY_BIRDS_GAME_BASE_H
