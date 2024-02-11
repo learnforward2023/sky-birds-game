73cd2e02c65aa973045836fa513444d896bdc9df
Learn Forward 2023 <learnforward2023@gmail.com>
2024-02-11 09:25:06 +0900

    Create header file Common.h

---
+++ Common.h
@@ -0,0 +1,9 @@
+//
+// Created by Study Together on 2024/02/11.
+// https://studytogether.vn
+//
+
+#ifndef SKY_BIRDS_GAME_COMMON_H
+#define SKY_BIRDS_GAME_COMMON_H
+
+#endif //SKY_BIRDS_GAME_COMMON_H
